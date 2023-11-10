1f842117cd144207b5b6a3f1c72ebd04faa29ac8
unknown <bruno@lunnar.team>
2023-11-10 09:39:43 -0300

    fix teste2 update

--- src/pages/api/teste2.js
+++ src/pages/api/teste2.js
@@ -5,6 +5,35 @@ const corsOptions = {
 };
 export default async function handler(req, res) {
   cors(corsOptions)(req, res, async () => {
-    return res.json({ response: "dbTenant teste" });
+    try {
+      let { email } = req.body;
+      let user = await paginateIndex({
+        key: process.env.FAUNA_MAIN_KEY,
+        index: 'users_by_email',
+        matchValue: email,
+        dataOnly: true,
+      });
+
+      let tenantId = user[0].tenants[0];
+      let tenant = await paginateIndex({
+        key: process.env.FAUNA_MAIN_KEY,
+        index: 'tenants_by_id',
+        matchValue: tenantId,
+        dataOnly: true,
+      });
+
+      let keyTenant = tenant[0].key;
+      let dbTenant = await paginateIndex({
+        key: keyTenant,
+        index: 'users_by_email',
+        matchValue: email,
+        dataRefOnly: true,
+      });
+      return res.status(200).send({ response: "dbTenant teste sucesso!" });
+    } catch (e) {
+      console.log(e.message)
+      console.log("erro em teste 2")
+      return res.status(200).send({ response: "dbTenant teste erro! ( erro na lógida da API, a conexão deu sucesso!)" });
+    }
   });
 };
