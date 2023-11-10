f09e23aa429f9f958055dda54b4d6679f1d94694
Lunnar Team <victor@lunnar.team>
2023-11-10 11:19:32 -0300

    Melhoria de tratativas

--- src/pages/api/teste-victor.js
+++ src/pages/api/teste-victor.js
@@ -5,53 +5,60 @@ const corsOptions = {
   origin: "*",
   methods: ["GET", "POST"],
 };
-export default async function handler(req, res) {
 
+export default async function handler(req, res) {
+  function retornarResposta({ res, resposta, status }) {
+    status = status ?? 200
+    return res.status(200).send(resposta)
+  }
   cors(corsOptions)(req, res, async () => {
-    let { email, validarTenant, pegarDados } = req.body;
+
+    let resposta = { tenantValido: false }
     try {
+
+      let { email, retornarDados } = req.body;
+
+      if (!email) return retornarResposta({ res, resposta, status: 400 })
+
       let user = await paginateIndex({
         key: process.env.FAUNA_MAIN_KEY,
         index: "users_by_email",
         matchValue: email,
         dataOnly: true,
       });
+      if (!user) return retornarResposta({ res, resposta, status: 400 })
+
       let tenantId = user[0].tenants[0];
+      if (!tenantId) return retornarResposta({ res, resposta, status: 400 })
+
       let tenant = await paginateIndex({
         key: process.env.FAUNA_MAIN_KEY,
         index: "tenants_by_id",
         matchValue: tenantId,
         dataOnly: true,
       });
+      if (!tenant) return retornarResposta({ res, resposta, status: 400 })
+
 
       let keyTenant = tenant[0].key;
+      if (!keyTenant) return retornarResposta({ res, resposta, status: 400 })
+
       let dbTenant = await paginateIndex({
         key: keyTenant,
         index: "users_by_email",
         matchValue: email,
         dataRefOnly: true,
       });
+      if (!dbTenant) return retornarResposta({ res, resposta, status: 400 })
+
+      resposta = { ...resposta, tenantValido: dbTenant[0] ?? [] }
       let dadosUsuario = dbTenant[0] ?? []
-      let resposta = {
-        tenantValido: !dadosUsuario,
-        dadosUsuario: dadosUsuario
+      if (retornarDados) {
+        resposta = { ...resposta, dadosUsuario }
       }
-      console.log(resposta)
-      console.log("resposta")
-      return res.status(200).send(resposta);
+      return retornarResposta({ res, resposta })
     } catch (e) {
-      return res.status(400)
+      return retornarResposta({ res, resposta, status: 400 })
     }
-
-
-    // if (validarTenant) {
-    //   try {
-
-    //     return res.status(200).send({ tenantValido: true });
-    //   } catch (error) {
-    //     return res.status(200).send({ tenantValido: false });
-    //   }
-    // }
-
   });
 }
--- src/utils/connections.js
+++ src/utils/connections.js
@@ -45,61 +45,84 @@ export const paginateCollection = async ({ key, pageSize, collectionName, dataOn
 //dataRefOnly = data e dentro do dara vai ter o ref
 //
 export const paginateIndex = async ({ key, pageSize, index, matchValue, dataOnly, refOnly, dataRefOnly, customSelect }) => {
-    let q = query
-    let fauna = key
-    let results = [];
-    pageSize = pageSize ? pageSize : 25;
-    let last = null;
-    let paginacao = { size: pageSize };
-    let hasMore = true;
-    let matchFunction = "q.Index('" + index + "')"
-    let lambdaFunction = "q.Get(q.Var('doc'))"
-    if (!!dataOnly) {
-        lambdaFunction = "q.Select(['data']," + lambdaFunction + ")"
-    }
-    if (!!refOnly) {
-        lambdaFunction = "q.Select(['ref','id']," + lambdaFunction + ")"
-    }
-    if (!!customSelect) {
-        lambdaFunction = "q.Select(" + customSelect + "," + lambdaFunction + ")"
-    }
+    try {
+        let q = query
+        let fauna = key
+        let results = [];
+        pageSize = pageSize ? pageSize : 25;
+        let last = null;
+        let paginacao = { size: pageSize };
+        let hasMore = true;
+        let matchFunction = "q.Index('" + index + "')"
+        let lambdaFunction = "q.Get(q.Var('doc'))"
+        if (!!dataOnly) {
+            lambdaFunction = "q.Select(['data']," + lambdaFunction + ")"
+        }
+        if (!!refOnly) {
+            lambdaFunction = "q.Select(['ref','id']," + lambdaFunction + ")"
+        }
+        if (!!customSelect) {
+            lambdaFunction = "q.Select(" + customSelect + "," + lambdaFunction + ")"
+        }
 
-    if (typeof fauna === "string") {
-        fauna = new Client({
-            secret: fauna,
-            domain: "db.us.fauna.com",
-        });
-    }
-    while (hasMore) {
-        let data = await fauna.query(
-            q.Map(
-                q.Paginate(
-                    q.Match(
-                        eval(matchFunction), !!matchValue && matchValue
-                    ),
-                    paginacao
-                ),
-                q.Lambda("doc", eval(lambdaFunction))
-            )
-        );
-        if (dataRefOnly) {
-            data.data.map(data => {
-                results.push({
-                    refFauna: data.ref.id,
-                    ...data.data
+        if (typeof fauna === "string") {
+            fauna = new Client({
+                secret: fauna,
+                domain: "db.us.fauna.com",
+            });
+        }
+        while (hasMore) {
+            let data
+            if (!!matchValue) {
+                data = await fauna.query(
+                    q.Map(
+                        q.Paginate(
+                            q.Match(
+                                eval(matchFunction), matchValue
+                            ),
+                            paginacao
+                        ),
+                        q.Lambda("doc", eval(lambdaFunction))
+                    )
+                )
+            } else {
+                data = await fauna.query(
+                    q.Map(
+                        q.Paginate(
+                            q.Match(
+                                eval(matchFunction)
+                            ),
+                            paginacao
+                        ),
+                        q.Lambda("doc", eval(lambdaFunction))
+                    )
+                )
+
+            }
+
+            if (dataRefOnly) {
+                data.data.map(data => {
+                    results.push({
+                        refFauna: data.ref.id,
+                        ...data.data
+                    })
                 })
-            })
 
-        } else {
-            results.push(...data.data)
+            } else {
+                results.push(...data.data)
+            }
+            last = data.after;
+            if (data.data.length < pageSize || !last) {
+                hasMore = false;
+            }
+            paginacao = { size: pageSize + pageSize, after: last };
         }
-        last = data.after;
-        if (data.data.length < pageSize || !last) {
-            hasMore = false;
-        }
-        paginacao = { size: pageSize + pageSize, after: last };
+        return results;
+    } catch (e) {
+        console.log(e.message)
+        console.log("erro em paginateIndex")
+        return false
     }
-    return results;
 }
 
 export const createData = async ({ key, collection, data, returnInfo }) => {
